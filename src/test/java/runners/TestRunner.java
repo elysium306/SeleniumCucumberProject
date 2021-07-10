bf13955311e18a67277e6026a62143ba27ae6d54
helil-primetech <helil@primetechschool.com>
2021-07-10 17:51:31 -0400

    added api feature and steps

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		features="./src/test/resources/features",
 		glue="step_definitions",
 		dryRun=false,
-		tags="@loginValidTest",
+		tags="@FindByStatus",
 		publish=true
 		)
 public class TestRunner {
