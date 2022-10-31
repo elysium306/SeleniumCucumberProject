566f2393a00b786589887cffcc81cf99d903bd51
elysium306 <elysium306@hotmail.com>
2022-10-30 21:16:18 -0500

    Added new test cases updated properties file added new dependencies to pom.xml

---
+++ src/main/java/com/example/seleniumcucumberproject/SeleniumCucumberProjectApplication.java
@@ -0,0 +1,17 @@
+package com.example.seleniumcucumberproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SeleniumCucumberProjectApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SeleniumCucumberProjectApplication.class, args);
+        var car = applicationContext.getBean(Car.class);
+        car.getCar();
+        System.out.println("Spring Boot 'main' Application Starting ...");
+    }
+
+}
