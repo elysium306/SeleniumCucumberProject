1717da0f8ac1c839732ba6a818c54667ac8e60e9
elysium306 <elysium306@hotmail.com>
2023-01-19 16:38:40 -0600

    Deleted some Spring folder(s)/file(s)

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceI.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.msscbrewery.services;
-
-import guru.springframework.msscbrewery.web.model.model.CustomerDTO;
-
-import java.util.UUID;
-
-public interface CustomerServiceI {
-    CustomerDTO getCustomerById(UUID customerId);
-}
